52a1daf6da84eafff5ece6f7e5d0e2950666d27c
codyreading <codyreading@gmail.com>
2023-05-06 13:39:27 -0400

    Tracked metrics and added benchmark for testing

--- benchmarking/benchmark_llff.sh
+++ benchmarking/benchmark_llff.sh
@@ -3,9 +3,16 @@
 export ROOT_DIR=/home/creading/Datasets/nerf_llff_data
 export DOWNSAMPLE=0.25 # to avoid OOM
 export SCALE=2.0
+export DATASET_NAME=colmap
+
+# python train.py \
+#     --root_dir $ROOT_DIR/fern \
+#     --exp_name fern --downsample $DOWNSAMPLE --no_save_test --scale $SCALE \
+#     --dataset_name $DATASET_NAME --num_epochs 30 --batch_size 4096 --eval_lpips
+
 
 python train.py \
     --root_dir $ROOT_DIR/fern \
-    --exp_name fern --downsample $DOWNSAMPLE --no_save_test \
-    --dataset_name colmap --num_epochs 30 --batch_size 8192 --eval_lpips
-
+    --exp_name fern --downsample $DOWNSAMPLE --val_only --scale $SCALE \
+    --ckpt_path ckpts/$DATASET_NAME/fern/epoch=29.ckpt \
+    --dataset_name $DATASET_NAME --num_epochs 30 --batch_size 4096 --eval_lpips
