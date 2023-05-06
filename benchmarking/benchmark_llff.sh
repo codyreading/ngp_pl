0ddc7f793bf4ada0b92c57d713e956cd6e9eeacb
codyreading <codyreading@gmail.com>
2023-05-06 11:41:13 -0400

    Updated dataset name

--- benchmarking/benchmark_llff.sh
+++ benchmarking/benchmark_llff.sh
@@ -7,5 +7,5 @@ export SCALE=2.0
 python train.py \
     --root_dir $ROOT_DIR/fern \
     --exp_name fern --downsample $DOWNSAMPLE --no_save_test \
-    --num_epochs 30 --batch_size 8192 --eval_lpips
+    --dataset_name colmap --num_epochs 30 --batch_size 8192 --eval_lpips
 
